26d4d57e2f01487ef4d39ceaf247e625a107c297
Nabeel Anjum <timedilation2012@gmail.com>
2023-10-15 01:39:31 +0500

    Update VideoPlayback.tsx

--- src/screens/VideoPlayback.tsx
+++ src/screens/VideoPlayback.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import YoutubePlayer from "react-native-youtube-iframe";
 
-import AppText from "../components/shared/AppText";
 import { SCREEN_HEIGHT, SCREEN_WIDTH } from "../common/constants";
 
 const VideoPlayback: React.FC = ({ route }) => {
